f2d8f59a5bf2940e3a6d6a2fa496a6b18d30c741
Ariana Amiri <amiriari@usc.edu>
2025-02-01 06:11:02 +0000

    all done with part 1 :)

--- split.cpp
+++ split.cpp
@@ -11,13 +11,32 @@ the function below should be the only one in this file.
 */
 
 #include "split.h"
+#include <cstddef>
 
 /* Add a prototype for a helper function here if you need */
 
 void split(Node*& in, Node*& odds, Node*& evens)
 {
   /* Add code here */
-// WRITE YOUR CODE HERE
+  if (in ==NULL) {
+    return;
+  }
+
+  Node* nexNode = in->next;
+
+  if (in->value % 2 ==0) {
+    evens = in;
+    evens->next = NULL;
+
+    split(nexNode, odds, evens->next);
+  }
+  else {
+    odds = in;
+    odds->next = NULL;
+    split(nexNode, odds->next, evens);
+  }
+
+  in = NULL;
 }
 
 /* If you needed a helper function, write it here */
--- test_split.cpp
+++ test_split.cpp
@@ -11,7 +11,24 @@ g++ split.cpp test_split.cpp -o test_split
 
 #include "split.h"
 
+void deleteNode(Node* node) {
+  if (node == nullptr) {
+    return;
+  }
+  deleteNode(node->next);
+  delete node;
+}
+
 int main(int argc, char* argv[])
 {
+  Node* head = new Node{1, new Node{2, new Node{3, new Node{4, nullptr}}}};
+  Node* odds = nullptr;
+  Node* evens = nullptr;
+
+  split(head, odds, evens);
+
+  deleteNode(odds);
+  deleteNode(evens);
+
 
 }
